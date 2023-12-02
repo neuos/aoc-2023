376bb0ac9ebcd72e32643eb0a4013bcb2b86ae42
neuos <github@neuhuber.eu>
2023-12-02 11:07:00 +0100

    solve day 02 star 2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -15,16 +15,16 @@ object Day02 : Day(2) {
             val isPossible = rounds.all { round ->
                 round.all { (color, amount) ->
 
-                        val possible = when (color) {
-                            "red" -> amount <= red
-                            "green" -> amount <= green
-                            "blue" -> amount <= blue
-                            else -> error("Unknown color $color")
-                        }
-                        if (!possible) {
-                            println("Game $gameId is not possible because $color has $amount")
-                        }
-                        possible
+                    val possible = when (color) {
+                        "red" -> amount <= red
+                        "green" -> amount <= green
+                        "blue" -> amount <= blue
+                        else -> error("Unknown color $color")
+                    }
+                    if (!possible) {
+                        println("Game $gameId is not possible because $color has $amount")
+                    }
+                    possible
 
                 }
             }
@@ -42,7 +42,34 @@ object Day02 : Day(2) {
     }
 
     override fun solvePart2(input: Sequence<String>): Any {
-        TODO("Not yet implemented")
+        return input.filter { it.isNotBlank() }.map { it.split(':') }.map { (gamename, roundsString) ->
+            val gameId = gamename.split(' ').last().toInt()
+            val rounds = roundsString.split(';').map { round ->
+                round.split(',').map {
+                    val (amount, color) = it.trim().split(' ')
+                    color to amount.toInt()
+                }
+            }
+            var minRed = 0
+            var minGreen = 0
+            var minBlue = 0
+
+            rounds.forEach() { round ->
+                round.forEach() { (color, amount) ->
+                    when (color) {
+                        "red" -> if (amount > minRed) minRed = amount
+                        "green" -> if (amount > minGreen) minGreen = amount
+                        "blue" -> if (amount > minBlue) minBlue = amount
+                        else -> error("Unknown color $color")
+                    }
+                }
+            }
+            println(rounds)
+            println("Game $gameId is possible with $minRed red, $minGreen green and $minBlue blue")
+
+            val power = minRed * minGreen * minBlue
+            power
+        }.sum()
     }
 
     override val expected = DayResult(8, 2101, 2286, 0)
