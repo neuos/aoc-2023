35dae10bdddd1b9b06cc8bebbef18b41dfe93030
neuos <github@neuhuber.eu>
2023-12-03 13:35:35 +0100

    solve day 2 part 2

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,5 +1,5 @@
 object Day03 : Day(3) {
-    override val expected = DayResult(4361L, 0L)
+    override val expected = DayResult(4361L, 539433L, 467835, 75847567)
 
     override fun solvePart1(input: Sequence<String>): Long {
         val list = input.toList()
@@ -32,11 +32,39 @@ object Day03 : Day(3) {
 
 
     override fun solvePart2(input: Sequence<String>): Any {
-        TODO("Not yet implemented")
+        val list = input.toList()
+        val grid = list.charGrid()
+        val gears = mutableMapOf<Pair<Int, Int>, List<Int>>()
+        list.flatMapIndexed { row, s ->
+
+            s.indexedNumbers()
+            val numbers = s.split(Regex("\\D")).filter { it.isNotEmpty() }
+            println("row: $row numbers: $numbers")
+            var index = 0
+            numbers.map { num ->
+                index = s.indexOf(num, index)
+                println("found at $index")
+                assert(index >= 0)
+                val range = index..<index + num.length
+                index += num.length
+
+                val numberGears: Sequence<Pair<Int, Int>> = grid.adjacentIndices(row, range).filter { (row, col) ->
+                    grid[row][col].isGear()
+                }
+                numberGears.forEach { (row, col) ->
+                    gears[row to col] = gears.getOrDefault(row to col, emptyList()) + num.toInt()
+                }
+            }
+        }
+        println(gears)
+        return gears.values.filter { it.size == 2 }.map { (a,b) ->a*b  }.sum()
     }
 
 
 }
+
+private fun Char.isGear() = equals('*')
+
 
 private fun String.indexedNumbers() {
     // find all numbers and their start and end index
@@ -51,6 +79,22 @@ private fun String.indexedNumbers() {
 // too low 538335
 
 // too high 539518
+
+
+private fun <E> List<List<E>>.adjacentIndices(row: Int, range: IntRange) = sequence {
+    // left and right
+    yield(row to range.first - 1)
+    yield(row to range.last + 1)
+
+    // top and bottom
+    for (i in listOf(row - 1, row + 1)) {
+        for (j in range.first - 1..range.last + 1) {
+            yield(i to j)
+        }
+    }
+}.filter { (row, col) ->
+    row in indices && col in this[row].indices
+}
 
 private fun <E> List<List<E>>.adjacent(row: Int, columns: IntRange) = sequence<E> {
     // left and right
