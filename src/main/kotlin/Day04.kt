9939c1cf61a8492ee5381b3448fad82aa107de8f
neuos <github@neuhuber.eu>
2023-12-04 09:28:34 +0100

    solve day 04 part 2

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,12 +1,12 @@
 import kotlin.math.pow
 
 object Day04 : Day(4) {
-    override val expected = DayResult(13, 0)
+    override val expected = DayResult(13, 22193, 30, 0)
     override fun solvePart1(input: Sequence<String>): Any {
         return input.filter { it.isNotBlank() }.map {
             it.split(':')
-        }.map { (name, values)->
-            values.split(" | ").map { it.split(" ").filter {it.isNotEmpty() }.map { it.toInt() }.toSet() }
+        }.map { (name, values) ->
+            values.split(" | ").map { it.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.toSet() }
         }.map { (winning, have) ->
             println("$have $winning")
             val filter = have.filter { it in winning }
@@ -14,11 +14,39 @@ object Day04 : Day(4) {
             filter.size
         }.filter { it > 0 }.map {
             println(it)
-            2.0.pow(it-1).toInt() }.sum()
+            2.0.pow(it - 1).toInt()
+        }.sum()
     }
 
     override fun solvePart2(input: Sequence<String>): Any {
-        TODO("Not yet implemented")
+        val cards = input.filter { it.isNotBlank() }.map {
+            it.split(':')
+        }.map { (name, values) ->
+            values.split(" | ").map { it.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.toSet() }
+        }.map { (winning, have) ->
+//            println("$have $winning")
+            val filter = have.filter { it in winning }
+//            println(filter)
+            filter.size
+        }.mapIndexed { index, winCount ->
+            index to if (winCount > 0) (index + 1..index + winCount) else emptyList()
+        }.sortedBy { it.second.count() }.toList()
+
+        val total = mutableMapOf<Int, Int?>()
+        while (cards.any { it.first !in total }){
+            cards.filter { it.first !in total }.forEach { (index, cards) ->
+                val winnings = cards.map { total[it] }
+                println("card $index cards $cards winnings $winnings")
+                if (!winnings.any { it == null }) {
+                    total[index] = winnings.filterNotNull().sum() + 1
+                    println("$index ${total[index]}")
+                }
+            }
+        }
+
+        println(total)
+        println(total.values.sumOf { it!! })
+        return total.values.sumOf { it!! }
     }
 
 
