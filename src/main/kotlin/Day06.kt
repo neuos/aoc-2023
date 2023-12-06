1ecf1acf04d1d906ef49fb6b216ee50221e96164
neuos <github@neuhuber.eu>
2023-12-06 08:40:00 +0100

    cleanup and refactor day 06

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,27 +1,23 @@
 object Day06 : Day(6) {
-    override val expected = DayResult(288, 316800, 71503, 45647654L)
+    override val expected = DayResult(288, 316800, 71503, 45647654)
     override fun solvePart1(input: Sequence<String>): Any {
-        val list = input.toList()
-        val times = list.first().split(Regex("\\D")).filter { it.isNotEmpty() }.map { it.toLong() }
-        val distances = list.last().split(Regex("\\D")).filter { it.isNotEmpty() }.map { it.toLong() }
+        val (timeLine, distanceLine) = input.toList()
+        val times = timeLine.split(Regex("\\D")).filter { it.isNotEmpty() }.map { it.toLong() }
+        val distances = distanceLine.split(Regex("\\D")).filter { it.isNotEmpty() }.map { it.toLong() }
 
         return times.zip(distances).map { (time, distance) ->
             waysToWin(time, distance)
-        }.reduce { a, b -> a * b }
+        }.reduce(Int::times)
     }
 
     override fun solvePart2(input: Sequence<String>): Int {
-        val list = input.toList()
-        val time = list.first().filter { it.isDigit() }.toLong()
-        val distance = list.last().filter { it.isDigit() }.toLong()
+        val (timeLine, distanceLine) = input.toList()
+        val time = timeLine.filter { it.isDigit() }.toLong()
+        val distance = distanceLine.filter { it.isDigit() }.toLong()
         return waysToWin(time, distance)
     }
 
-    private fun waysToWin(time: Long, distance: Long) = (1..<time).map { v ->
-        v * (time - v)
-    }.count { it > distance }
-}
-
-fun main() {
-    print(Day06.part2())
+    private fun waysToWin(time: Long, distance: Long) = (1..<time).count { v ->
+        (v * (time - v)) > distance
+    }
 }
