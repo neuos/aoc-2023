aaa09975eb32faf77699fee2c33b75037edf524d
neuos <github@neuhuber.eu>
2023-12-08 15:20:43 +0100

    solve day 08 part 2 with lgm math

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,28 +1,14 @@
 object Day08 : Day(8) {
-    override val expected = DayResult(2, 13939, 6, "TODO")
+    override val expected = DayResult(2, 13939, 6L, "TODO")
     override fun solvePart1(input: Sequence<String>): Any {
-        val list = input.toList()
-        val instructions = list.first().toCharArray()
-        val nodes = list.drop(1).filter { it.isNotEmpty() }.map { Node.from(it) }.associateBy { it.name }
+        val (instructions, nodes) = parseInput(input)
 
         var i = 0
-        var current = "AAA"
-        val end = "ZZZ"
+        var current = nodes["AAA"]!!
+        val end = nodes["ZZZ"]!!
 
         while (current != end) {
-            val node = nodes[current]!!
-            val left = nodes[node.left]!!
-            val right = nodes[node.right]!!
-            val instruction = instructions[i % instructions.size]
-            when (instruction) {
-                'L' -> {
-                    current = left.name
-                }
-
-                'R' -> {
-                    current = right.name
-                }
-            }
+            current = applyInstruction(nodes, current, instructions[i % instructions.size])
             i++
         }
 
@@ -30,28 +16,108 @@ object Day08 : Day(8) {
         return i
     }
 
+    private fun applyInstruction(
+        nodes: Map<String, Node>, current: Node, instruction: Char
+    ) = when (instruction) {
+        'L' -> nodes[current.left]!!
+        'R' -> nodes[current.right]!!
+        else -> error("Unknown instruction $instruction")
+    }
+
     override fun solvePart2(input: Sequence<String>): Any {
-        val list = input.toList()
-        val instructions = list.first().toCharArray()
-        val nodes = list.drop(1).filter { it.isNotEmpty() }.map { Node.from(it) }.associateBy { it.name }
+        val (instructions, nodes) = parseInput(input)
 
-        var i = 0
-        fun isEnd(node: Node) = node.name.endsWith('Z')
-        var current = nodes.values.filter { it.name.endsWith('A') }
 
-        while (current.any { !isEnd(it) }) {
-            current = current.map { node ->
-                when (val instruction = instructions[i % instructions.size]) {
-                    'L' -> nodes[node.left] ?: error("Unknown node ${node.left}")
-                    'R' -> nodes[node.right] ?: error("Unknown node ${node.right}")
-                    else -> error("Unknown instruction $instruction")
+        val startingNodes = nodes.values.filter { it.name.endsWith('A') }
+        val loops = startingNodes.map { findPath(nodes, it, instructions) }
+
+        val canUseLGM = loops.all { path -> path.loopEnds.keys.singleOrNull() == path.loopLength }
+        if (canUseLGM) {
+            return loops.map { it.ends.keys.single().toLong() }.reduce { acc, i -> leastCommonMultiple(acc, i) }
+
+        } else {
+            val ends = loops.map { it.endIndices().asIterable().iterator() }
+            while (true) {
+                val next = ends.map { it.next() }
+//            if (next.first() > 1000) return 0
+                println("next $next")
+                if (next.allEquals()) {
+                    println(next.first())
+                    return next.first()
+                }
+                val max = next.max()
+                ends.filterIndexed { index, _ -> next[index] < max }.forEach { end ->
+                    end.takeWhile { it < max }
                 }
             }
+        }
+
+
+    }
+
+    private fun leastCommonMultiple(a: Long, b: Long) = a * b / greatestCommonDivisor(a, b)
+
+    private tailrec fun greatestCommonDivisor(a: Long, b: Long): Long = if (b == 0L) a
+    else greatestCommonDivisor(b, a % b)
+
+    private fun <T> Iterator<T>.takeWhile(condition: (T) -> Boolean) {
+        while (condition(next())) {
+        }
+    }
+
+    private fun <T> Iterable<T>.allEquals() = firstOrNull()?.let { first -> drop(1).all { it == first } } ?: true
+
+    fun isEnd(node: Node) = node.name.endsWith('Z')
+    fun findPath(nodes: Map<String, Node>, start: Node, instructions: CharArray): Path {
+        var i = 0
+        var current = start
+        val visited = mutableSetOf<Pair<Node, Int>>()
+        val ends = mutableMapOf<Int, Node>()
+
+        while (current to i.mod(instructions.size) !in visited) {
+            if (isEnd(current)) {
+                ends[i] = current
+            }
+
+            val instruction = instructions[i.mod(instructions.size)]
+            visited.add(current to i.mod(instructions.size))
+            current = applyInstruction(nodes, current, instruction)
             i++
         }
 
-        println(i)
-        return i
+        i = i.mod(instructions.size)
+        val inLoop = mutableSetOf<Pair<Node, Int>>()
+        while (current to i !in inLoop) {
+            inLoop.add(current to i)
+            current = applyInstruction(nodes, current, instructions[i])
+            i = (i + 1).mod(instructions.size)
+        }
+
+        val loopLength = inLoop.size
+        val preLoopLength = visited.size - loopLength
+//        println("visited $visited")
+//        println("inLoop $inLoop")
+        return Path(preLoopLength, loopLength, ends)
+    }
+
+    data class Path(val preLoopLength: Int, val loopLength: Int, val ends: Map<Int, Node>) {
+        val loopEnds = ends.filter { it.key >= preLoopLength }
+        val length = preLoopLength + loopLength
+
+        override fun toString(): String {
+            return "Path(preLoopLength=$preLoopLength, loopLength=$loopLength, ends=$ends, loopEnds=$loopEnds, length=$length)"
+        }
+
+        fun endIndices() = generateSequence(0L) { it + 1 }.flatMap { i ->
+            ends.keys.map { it + loopLength * i }
+        }
+    }
+
+    private fun parseInput(input: Sequence<String>): Pair<CharArray, Map<String, Node>> {
+        val list = input.toList()
+        val instructions = list.first().toCharArray()
+        val nodes = list.drop(1).filter { it.isNotEmpty() }.map { Node.from(it) }.associateBy { it.name }
+        return Pair(instructions, nodes)
     }
 }
 
@@ -62,4 +128,12 @@ data class Node(val name: String, val left: String, val right: String) {
             return Node(split[0], split[1], split[2])
         }
     }
+
+    override fun toString() = "$name($left, $right)"
+}
+
+fun main() {
+
+    Day08.part2Example()
+    print(Day08.part2())
 }
