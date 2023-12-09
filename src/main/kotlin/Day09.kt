eda70cc6582e841d724656cdaf43576f32a52e55
neuos <github@neuhuber.eu>
2023-12-09 07:32:06 +0100

    solve day 09 part 1

---
+++ src/main/kotlin/Day09.kt
@@ -0,0 +1,28 @@
+object Day09 : Day(9) {
+    override val expected = DayResult(114, "TODO", "TODO", "TODO")
+    override fun solvePart1(input: Sequence<String>): Any {
+        return input.map {
+            it.split(" ").map { it.toInt() }
+        }.map {
+            val diffs = mutableListOf<List<Int>>(it)
+            do {
+                val diff = diffs.last().windowed(2).map { it[1] - it[0] }
+                println("diff: $diff")
+                diffs.add(diff)
+            } while (diff.any { it != 0 })
+            println(diffs)
+
+            var next = 0
+            diffs.indices.reversed().forEach { i ->
+                val diff = diffs[i]
+                next += diff.last()
+            }
+            println(next)
+            next
+        }.sum()
+    }
+
+    override fun solvePart2(input: Sequence<String>): Any {
+        return 0
+    }
+}
