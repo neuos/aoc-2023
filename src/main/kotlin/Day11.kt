9f92c20acc7d6c1c907ab1e2c7e3c41f9001e1ed
neuos <github@neuhuber.eu>
2023-12-12 11:33:04 +0100

    solve day 11 part 2

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -3,18 +3,18 @@ import kotlin.math.abs
 private const val GALAXY = '#'
 
 object Day11 : Day(11) {
-    override val expected = DayResult(374, "TODO", "TODO", "TODO")
+    override val expected = DayResult(374, 10289334, "TODO", "TODO")
     override fun solvePart1(input: Sequence<String>): Any {
         val grid = input.map { it.toList() }.toList()
         val noGalaxyRows = grid.map { row -> row.all { it != GALAXY } }
         val noGalaxyColumns = grid.first().indices.map { column -> grid.all { row -> row[column] != GALAXY } }
 
         val rowOffsets = noGalaxyRows.mapIndexed { index, b -> if (b) index else null }.filterNotNull()
-        val rowIndices = grid.indices.map { row -> row + rowOffsets.filter { it < row }.size }
+        val rowIndices = grid.indices.map { row -> row + rowOffsets.filter { it < row }.size.toLong() }
 
 
         val columnOffsets = noGalaxyColumns.mapIndexed { index, b -> if (b) index else null }.filterNotNull()
-        val columnIndices = grid.first().indices.map { column -> column + columnOffsets.filter { it < column }.size }
+        val columnIndices = grid.first().indices.map { column -> column + columnOffsets.filter { it < column }.size.toLong() }
 
         println("noGalaxyRows: $noGalaxyRows")
         println("rowOffsets: $rowOffsets")
@@ -34,16 +34,45 @@ object Day11 : Day(11) {
 
         return coordinates.pairs().map { (a, b) ->
             manhattanDistance(a, b)
-        }.reduce(Int::plus)
+        }.reduce(Long::plus)
     }
 
     private fun manhattanDistance(a: Coordinate, b: Coordinate) = abs(a.x - b.x) + abs(a.y - b.y)
 
-    override fun solvePart2(input: Sequence<String>): Any {
-        return 0
+    override fun solvePart2(input: Sequence<String>): Long {
+        val grid = input.map { it.toList() }.toList()
+        val noGalaxyRows = grid.map { row -> row.all { it != GALAXY } }
+        val noGalaxyColumns = grid.first().indices.map { column -> grid.all { row -> row[column] != GALAXY } }
+
+        val rowOffsets = noGalaxyRows.mapIndexed { index, b -> if (b) index else null }.filterNotNull()
+        val age = 1000000L-1
+        val rowIndices = grid.indices.map { row -> row + rowOffsets.filter { it < row }.size * age }
+
+        val columnOffsets = noGalaxyColumns.mapIndexed { index, b -> if (b) index else null }.filterNotNull()
+        val columnIndices = grid.first().indices.map { column -> column + columnOffsets.filter { it < column }.size * age }
+
+        println("noGalaxyRows: $noGalaxyRows")
+        println("rowOffsets: $rowOffsets")
+        println("rowIndices: $rowIndices")
+
+        println("noGalaxyColumns: $noGalaxyColumns")
+        println("columnOffsets: $columnOffsets")
+        println("columnIndices: $columnIndices")
+
+        val coordinates = grid.flatMapIndexed { x, row ->
+            row.mapIndexedNotNull { y, c ->
+                if (c == GALAXY) Coordinate(rowIndices[x], columnIndices[y]) else null
+            }
+        }
+
+        println("coordinates: $coordinates")
+
+        return coordinates.pairs().map { (a, b) ->
+            manhattanDistance(a, b)
+        }.reduce(Long::plus)
     }
 
-    private data class Coordinate(val x: Int, val y: Int) : Comparable<Coordinate> {
+    private data class Coordinate(val x: Long, val y: Long) : Comparable<Coordinate> {
         override fun compareTo(other: Coordinate) = compareValuesBy(this, other, Coordinate::x, Coordinate::y)
         override fun toString() = "($x, $y)"
     }
