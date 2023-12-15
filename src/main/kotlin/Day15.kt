c252b15f6d8e3d7b910dcf2507a3fa66f0a8455a
neuos <github@neuhuber.eu>
2023-12-15 09:08:36 +0100

    day 15 solve part 2

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,16 +1,51 @@
 object Day15 : Day(15) {
-    override val expected = DayResult(1320, 504036, "TODO", "TODO")
+    override val expected = DayResult(1320, 504036, 145, 295719)
     override fun solvePart1(input: Sequence<String>): Any {
         return input.first().split(",").sumOf { it.hashAlgorithm() }
     }
 
+    data class Lens(val label: String, val focalLength: Int = 0) {
+        override fun equals(other: Any?) = other is Lens && other.label == label
+        override fun toString() = "$label $focalLength"
+    }
+
     override fun solvePart2(input: Sequence<String>): Any {
-        return 0
+        val boxes = List(256) { mutableListOf<Lens>() }
+        input.first().split(",").forEach {
+            if (it.contains("-")) {
+                val label = it.substringBefore("-")
+                val hash = label.hashAlgorithm()
+                val removed = boxes[hash].removeIf { it.label == label }
+//                assert(removed)
+            } else if (it.contains("=")) {
+                val label = it.substringBefore("=")
+                val focalLength = it.substringAfter("=").toInt()
+                val hash = label.hashAlgorithm()
+                boxes[hash].addOrReplace(Lens(label, focalLength))
+            }
+
+            println("After $it:")
+            boxes.forEachIndexed { index, lenses -> if (lenses.isNotEmpty()) println("Box $index: $lenses") }
+        }
+        return boxes.mapIndexed { boxIndex, lenses ->
+            lenses.mapIndexed { lensIndex, lens ->
+                (boxIndex+1) * (lensIndex+1) * lens.focalLength
+            }.sum()
+        }.sum()
     }
 }
 
-private fun String.hashAlgorithm(): Long {
-    var hash = 0L
+private fun MutableList<Day15.Lens>.addOrReplace(lens: Day15.Lens) {
+    val index = indexOf(lens)
+    if (index == -1) {
+        add(lens)
+    } else {
+        set(index, lens)
+    }
+}
+
+private fun String.hashAlgorithm(): Int {
+    var hash = 0
     for (i in indices) {
         hash = ((hash + get(i).code) * 17).rem(256)
     }
