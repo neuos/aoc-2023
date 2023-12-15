5834d989f7ebddfdd9498e8c4d1ae62284100ec9
neuos <github@neuhuber.eu>
2023-12-15 10:10:59 +0100

    day 14 not converting to string to list and back all of the time

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -2,6 +2,9 @@ import util.CharGrid
 import util.rotateAnticlockwise
 import util.rotateClockwise
 import util.toGrid
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.collections.set
 
 object Day14 : Day(14) {
     override val expected = DayResult(136, 109596, 64, 96105)
@@ -33,10 +36,19 @@ object Day14 : Day(14) {
     }
 
     private fun CharGrid.tiltGridLeft() = map { it.tiltLineLeft() }
-    private fun List<Char>.tiltLineLeft() = joinToString("").split("#").joinToString("#") {
-        it.toList().sortedDescending().joinToString("")
-    }.toList()
 
+    private fun List<Char>.tiltLineLeft() = split('#').map {
+        it.sortedDescending()
+    }.reduce { a, b -> a + '#' + b }
 
-
+    private fun List<Char>.split(char: Char): List<List<Char>> = buildList {
+        add(mutableListOf())
+        this@split.forEach {
+            if (it == char) {
+                this.add(mutableListOf())
+            } else {
+                this.last().addLast(it)
+            }
+        }
+    }
 }
